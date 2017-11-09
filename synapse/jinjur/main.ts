0ad0b9493bf6b894004968b164819cb73b11404e
Mike Ryan <mike.ryan52@gmail.com>
2017-11-09 15:11:13 -0600

    Rename jinjar to jinjur

--- synapse/jinjur/main.ts
+++ synapse/jinjur/main.ts
@@ -1,4 +1,4 @@
 import {platformBrowser} from '@angular/platform-browser';
-import {AppModuleNgFactory} from 'synapse/jinjar/app.ngfactory';
+import {AppModuleNgFactory} from 'synapse/jinjur/app.ngfactory';
 
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
