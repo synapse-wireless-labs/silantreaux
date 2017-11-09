b3ea067f84447e5e86b98e144f15e1879ea0fb84
Mike Ryan <mike.ryan52@gmail.com>
2017-11-09 14:43:53 -0600

    Add TypeScript testing

---
+++ synapse/demo-ui/hello-world/hello-world.component.spec.ts
@@ -0,0 +1,12 @@
+describe('Hello World Component', () => {
+
+  it('should compile successfully', () => {
+    expect(123).toBeDefined();
+  });
+
+  it('should prove Tyler wrong', () => {
+    const tylerIsKindOfRight = true;
+
+    expect(tylerIsKindOfRight).toBe(true);
+  });
+});
