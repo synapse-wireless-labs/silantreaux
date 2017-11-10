a379837001275eba702628be0d183b78c411561b
Mike Ryan <mike.ryan52@gmail.com>
2017-11-10 13:34:23 -0600

    Code cleanup from demo

--- synapse/jinjur/hello-world/hello-world.component.ts
+++ synapse/jinjur/hello-world/hello-world.component.ts
@@ -3,7 +3,7 @@ import {Component, NgModule} from '@angular/core';
 @Component({
   selector: 'hello-world-app',
   template: `
-    <div>Hello {{ name }}</div>
+    <div>Hello {{ name }}!!</div>
     <input type="text" [value]="name" (input)="name = $event.target.value"/>
   `,
   styleUrls: ['./hello-world-styles.css']
--- synapse/papreekuh/papreekuh/main.py
+++ synapse/papreekuh/papreekuh/main.py
@@ -1,5 +1,5 @@
 def cool_func():
-    return "BLAH"
+    return "BLAH!!"
 
 if __name__ == "__main__":
     print cool_func()
